4d3bdc34b9123abcb5367c9da57e961526a7b53f
Arcia125 <tauren125@gmail.com>
2016-11-17 18:38:25 -0600

    add homepage

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-	res.send(`Main page`);
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const getTime = (inputTime) => {
